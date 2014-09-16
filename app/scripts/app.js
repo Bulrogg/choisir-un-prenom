565bffef6c98395bd7f09f6b1084adfb25c97231
Frederic Millard <fmi@octo.com>
2014-09-16 21:46:29 +0200

    generation yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name choisirUnPrenomApp
+ * @description
+ * # choisirUnPrenomApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('choisirUnPrenomApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name choisirUnPrenomApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the choisirUnPrenomApp
+ */
+angular.module('choisirUnPrenomApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
