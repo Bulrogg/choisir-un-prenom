beb7aeb9af0f4e23330c8dcafd9aafc62c66994d
Frederic Millard <fmi@octo.com>
2014-09-22 22:16:29 +0200

    un premier service angular pour récupérer les prénoms

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,44 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name choisirUnPrenomApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the choisirUnPrenomApp
- */
 angular.module('choisirUnPrenomApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+	.controller('choisirUnPrenomCtrl', ['$scope', 'donneMoiDesPrenoms' ,function ($scope, donneMoiDesPrenoms) {
+
+		$scope.nom = 'Millard';
+		$scope.sexe = 'FILLE';
+		$scope.prenomPropose = '';
+		$scope.listeDesPrenomsGardes = [];
+		$scope.listeDesPrenomsRejetes = [];
+		$scope.prenomAProposer = [];
+
+		$scope.onGarde = function() {
+			$scope.listeDesPrenomsGardes.push($scope.prenomPropose);
+			$scope.changerDePrenom();
+		};
+
+		$scope.poubelle = function() {
+			$scope.listeDesPrenomsRejetes.push($scope.prenomPropose);
+			$scope.changerDePrenom();
+		};
+
+		$scope.changerDePrenom = function () {
+			if($scope.prenomAProposer.length == 0) {
+				$scope.prenomAProposer = donneMoiDesPrenoms($scope.sexe);
+			}
+			$scope.prenomPropose = $scope.prenomAProposer.pop();
+		}
+
+		$scope.changerDePrenom();
+
+	}])
+
+
+	.factory('donneMoiDesPrenoms', function() {
+		return function(sexe) {
+			// TODO bien mélanger les prénoms
+			if(sexe === 'FILLE') return ['Paulette', 'Georgette', 'binette', 'Stoukette', 'Zezette'];
+			if(sexe === 'GARCON') return ['Paul', 'George', 'binou', 'Stouk', 'Ziz'];
+			return ['Paulette', 'Georgette', 'binette', 'Stoukette', 'Zezette', 'Paul', 'George', 'binou', 'Stouk', 'Ziz']
+		};
+	})
+;
